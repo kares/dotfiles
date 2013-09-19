c1644fc1b07eb74a22e63211fa4b4725ff8b0dbc
kares <self@kares.org>
2013-09-19 19:54:40 +0200

    all styled now - no need for js

--- js/google.com.js
+++ js/google.com.js
@@ -1,8 +1,8 @@
-(function($) {
+//(function($) {
 //
 //$('#gbzw').hide(); // top bar including g+ crap
 //$('#gbvg').hide(); // top side g+ personal crap
-$('#pmocntr,#pmocntr2').hide().empty(); // chrome promo
-$('#fll').css('visibility', 'hidden'); // advertising program
+//$('#pmocntr,#pmocntr2').hide().empty(); // chrome promo
+//$('#fll').css('visibility', 'hidden'); // advertising program
 //
-})(jQuery);
+//})();
